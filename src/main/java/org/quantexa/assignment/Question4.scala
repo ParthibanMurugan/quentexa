15d6cdc1594ea0d6f5649478709633aaef1252aa
ParthibanMurugan <58543397+ParthibanMurugan@users.noreply.github.com>
2022-09-15 18:32:59 +0800

    Update Question4.scala

--- src/main/java/org/quantexa/assignment/Question4.scala
+++ src/main/java/org/quantexa/assignment/Question4.scala
@@ -5,16 +5,13 @@ import org.apache.spark.sql.functions.{col, count, lit}
 import org.quantexa.assignment.common.SparkEnv
 
 object Question4 extends SparkEnv{
-  var joinColumn = Seq("flightId", "from", "to", "date")
+  val joinColumn = Seq("flightId", "from", "to", "date")
   val rows = "3"
 
-  var data = flightData.as("a").join(flightData.as("b"), joinColumn)
+  val data = flightData.as("a").join(flightData.as("b"), joinColumn)
     .where(col("a.passengerId").<(col("b.passengerId")))
     .select(col("a.*"),col("b.passengerId").as("pass_1"))
-
-  data.printSchema()
-
-  data = data.withColumnRenamed("passengerId", "pass_2")
+    .withColumnRenamed("passengerId", "pass_2")
     .groupBy("pass_1", "pass_2")
     .agg(count(lit(1)).as("numOfFlights"), functions.min("date").as("From"), functions.max("date").as("To"))
     .filter(s"numOfFlights > $rows")
